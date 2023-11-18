cd12867168d811e77e14f056e11b12abe1512352
Wisnu Hidayat <wisnubaldas@gmail.com>
2023-11-18 14:28:59 +0700

    setting template metronic laravel

---
+++ resources/mix/vendors/draggable/draggable.bundle.js
@@ -0,0 +1,15 @@
+// Draggable - a lightweight, responsive, modern drag & drop library: https://shopify.github.io/draggable/
+
+module.exports = [
+    'node_modules/@shopify/draggable/lib/es5/draggable.bundle.js',
+    'node_modules/@shopify/draggable/lib/es5/draggable.bundle.legacy.js',
+    'node_modules/@shopify/draggable/lib/es5/draggable.js',
+    'node_modules/@shopify/draggable/lib/es5/sortable.js',
+    'node_modules/@shopify/draggable/lib/es5/droppable.js',
+    'node_modules/@shopify/draggable/lib/es5/swappable.js',
+    'node_modules/@shopify/draggable/lib/es5/plugins.js',
+    'node_modules/@shopify/draggable/lib/es5/plugins/collidable.js',
+    'node_modules/@shopify/draggable/lib/es5/plugins/resize-mirror.js',
+    'node_modules/@shopify/draggable/lib/es5/plugins/snappable.js',
+    'node_modules/@shopify/draggable/lib/es5/plugins/swap-animation.js'
+];
